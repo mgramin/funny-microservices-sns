97a07b1b0ecab59f24b49e9ce4346ea1bea7e222
mgramin <mgramin@gmail.com>
2018-02-11 20:11:57 +0300

    #5

--- message-service/src/main/java/com/github/mgramin/funny_microservices_sns/message_service/App.java
+++ message-service/src/main/java/com/github/mgramin/funny_microservices_sns/message_service/App.java
@@ -1,14 +1,10 @@
 package com.github.mgramin.funny_microservices_sns.message_service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
-	@Autowired
-    MessageRepository repository;
 
 	public static void main(String[] args) {
         SpringApplication.run(App.class, args);
