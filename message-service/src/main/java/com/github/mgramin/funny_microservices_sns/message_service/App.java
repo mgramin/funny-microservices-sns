99db9dda189477d58c94e8553043bbd61efd21a5
mgramin <mgramin@gmail.com>
2018-02-20 00:47:42 +0300

    fix #9

--- message-service/src/main/java/com/github/mgramin/funny_microservices_sns/message_service/App.java
+++ message-service/src/main/java/com/github/mgramin/funny_microservices_sns/message_service/App.java
@@ -1,13 +1,36 @@
 package com.github.mgramin.funny_microservices_sns.message_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class App {
 
 	public static void main(String[] args) {
         SpringApplication.run(App.class, args);
 	}
 
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/message-service")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
 }
